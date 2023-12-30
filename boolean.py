5c6fbaa96bd234adab550b01c6ffbb4422a8e4a1
mahinjaman <mahinjaman434@gmail.com>
2023-12-31 00:10:15 +0600

    update some file

--- boolean.py
+++ boolean.py
@@ -1,8 +1,8 @@
 # Boolean Type variable
 # boolean type variable er valu 2 ta e hoy 1.True and 2. False
-bool1 = True;
-print(bool1);
-print(type(bool1));
-bool2 = False;
-print(bool2);
-print(type(bool2));
+# bool1 = True;
+# print(bool1);
+# print(type(bool1));
+# bool2 = False;
+# print(bool2);
+# print(type(bool2));
--- debugging.py
+++ debugging.py
@@ -2,4 +2,5 @@ num = 0;
 
 while num < 10:
     print(num);
+    if num == 5:
     num += 1;
