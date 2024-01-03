4bd75807fb9a746489cf93454785c6528a77bdf6
mahinjaman01 <mahinjaman434@gmail.com>
2024-01-04 00:33:33 +0600

    i learn regular expression then try and except and file

---
+++ TryAndExcept.py
@@ -0,0 +1,27 @@
+# Python Try Except
+
+'''
+The (try) block lets you test a block of code for errors.
+
+The (except) block lets you handle the error.
+
+The (else) block lets you execute code when there is no error.
+
+The (finally) block lets you execute code, regardless of the result of the try- and except blocks.
+'''
+
+
+try:
+    #jei code gula amader mone hobe eta error hobe oi code gula amra try er vitor rakhbo
+    print(y)
+
+except:
+    #jode amader code error hoy sei error bujhar jonno amra except er moddhe akta error massage diye dite pati
+    print("Vai age error thik koren")
+
+
+#eikhan thekei amra amader code chalai jaite pari amader ar kono error dibe na
+print("Give me some function");
+print("Give me some list[]");
+print("Give me some set{}");
+print("Give me some tuple()");
---
+++ file.py
@@ -0,0 +1,32 @@
+# # open the text.text file
+# # mode "r"
+# readText = open("text.text", "r"); # open the text file using open() function / file ke read korar jonno amra "r" read er short from use koresi
+#
+# #readText ke out pot korar jonno arekta function use korte hobe / function ti holo: read()
+#
+# print(readText.read());
+#
+#
+#
+# # create file
+#
+# newFile = open("create.htmlt", "w");
+# newFile.write("Hey subscribers Please Follow me")
+# print(newFile);
+#
+# # "a" - append
+#
+# cFile = open("create.text","a");
+#
+# cFile.write("Hello World \n"); # \n  eitar maddhome amader new text line by line add hobe ta sara amader ak line text er modddhei shb add hobe
+#
+
+
+# remove file
+import  os
+# os.remove("create.html"); # remove this file
+# os.remove("create.text");
+
+# remove folder
+
+# os.rmdir("Hello BD")
